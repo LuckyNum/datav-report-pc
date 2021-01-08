9bd40eab0f1ee673df278134f40e7369cd3575a4
lch <chenghao.li@hand-china.com>
2021-01-09 00:03:31 +0800

    使用vue-charts重构头部图表实现

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -4,7 +4,7 @@
     value="1,410,350"
   >
     <template>
-      <div id="total-users-chart" :style="{ width: '100%', height: '100%' }"></div>
+      <v-chart :options="getOptions()" />
     </template>
     <template v-slot:footer>
       <div class="total-users-footer">
@@ -25,82 +25,82 @@ import commonCardMixin from '../../mixins/commonCardMixin'
 export default {
   name: 'index',
   mixins: [commonCardMixin],
-  mounted () {
-    const chartDom = document.getElementById('total-users-chart')
-    const chart = this.$echarts.init(chartDom)
-    chart.setOption({
-      xAxis: {
-        show: false,
-        boundaryGap: false,
-        type: 'value'
-      },
-      yAxis: {
-        show: false,
-        type: 'category'
-      },
-      series: [{
-        type: 'bar',
-        stack: '总量',
-        data: [200],
-        itemStyle: {
-          color: '#45C946'
+  methods: {
+    getOptions () {
+      return {
+        xAxis: {
+          show: false,
+          boundaryGap: false,
+          type: 'value'
         },
-        barWidth: 10
-      }, {
-        type: 'bar',
-        stack: '总量',
-        data: [250],
-        itemStyle: {
-          color: '#eee'
-        }
-      }, {
-        type: 'custom',
-        stack: '总量',
-        data: [200],
-        renderItem: (params, api) => {
-          const value = api.value(0)
-          const endPoint = api.coord([value, 0])
-
-          return {
-            type: 'group',
-            position: endPoint,
-            children: [{
-              type: 'path',
-              shape: {
-                d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
-                x: -5,
-                y: -20,
-                width: 10,
-                height: 10,
-                layout: 'cover'
-              },
-              style: {
-                fill: '#45c946'
-              }
-            }, {
-              type: 'path',
-              shape: {
-                d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
-                x: -5,
-                y: 10,
-                width: 10,
-                height: 10,
-                layout: 'cover'
-              },
-              style: {
-                fill: '#45c946'
-              }
-            }]
+        yAxis: {
+          show: false,
+          type: 'category'
+        },
+        series: [{
+          type: 'bar',
+          stack: '总量',
+          data: [200],
+          itemStyle: {
+            color: '#45C946'
+          },
+          barWidth: 10
+        }, {
+          type: 'bar',
+          stack: '总量',
+          data: [250],
+          itemStyle: {
+            color: '#eee'
           }
+        }, {
+          type: 'custom',
+          stack: '总量',
+          data: [200],
+          renderItem: (params, api) => {
+            const value = api.value(0)
+            const endPoint = api.coord([value, 0])
+
+            return {
+              type: 'group',
+              position: endPoint,
+              children: [{
+                type: 'path',
+                shape: {
+                  d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
+                  x: -5,
+                  y: -20,
+                  width: 10,
+                  height: 10,
+                  layout: 'cover'
+                },
+                style: {
+                  fill: '#45c946'
+                }
+              }, {
+                type: 'path',
+                shape: {
+                  d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
+                  x: -5,
+                  y: 10,
+                  width: 10,
+                  height: 10,
+                  layout: 'cover'
+                },
+                style: {
+                  fill: '#45c946'
+                }
+              }]
+            }
+          }
+        }],
+        grid: {
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
         }
-      }],
-      grid: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
       }
-    })
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import ECharts from 'echarts'
+import VueECharts from 'vue-echarts'
+// 由于需要覆盖原有样式，需要在最后引入
+import './style/index.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$echarts = ECharts
+Vue.component('v-chart', VueECharts)
 
 new Vue({
   router,
